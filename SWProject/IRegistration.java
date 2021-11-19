740950b6467b4e94b0cecd470984460e8a1f4c86
hossamelgendy1 <59685428+hossamelgendy1@users.noreply.github.com>
2021-11-19 20:59:51 +0200

    Update IRegistration.java

--- SWProject/IRegistration.java
+++ SWProject/IRegistration.java
@@ -1,13 +1,6 @@
 package SWProject;
 
 public interface IRegistration {
-    public void setDriver(IDriver driver);
-    public void setLicenseId(String licenseId);
-    public void setNationalId(String nationalId);
-    public String getNationalId();
-    public String getLicenseId();
-    public IDriver getDriver();
-
     public void setUserInfo(UserInfo info);
     public UserInfo getUserInfo();
     public void setAccepted(boolean isAccepted);
