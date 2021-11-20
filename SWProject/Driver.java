47ea6718647eb4459a757abdd01986746634b58f
hossamelgendy1 <59685428+hossamelgendy1@users.noreply.github.com>
2021-11-20 16:32:26 +0200

    Update Driver.java

--- SWProject/Driver.java
+++ SWProject/Driver.java
@@ -2,4 +2,40 @@ package SWProject;
 
 public class Driver implements IDriver, IUser {
 
+    @Override
+    public void setSuspended(boolean isSuspended) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public UserInfo getPersonalInfo() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean hasFavouriteArea(String area) {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public void recieveRideNotification(IRide ride) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public double getAverageRate() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void recieveRating(IRating rating) {
+        // TODO Auto-generated method stub
+        
+    }
+
 }
